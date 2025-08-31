c97be6df5bb2f18f00bc23e22e3470483536e3af
Yana Khovbosha <aemec4373@gmail.com>
2025-08-31 23:25:26 +0300

    fix build

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview";
+import NotePreview from "@/app/@modal/(.)notes/[id]/PageOne";
 
 interface Props {
   params: Promise<{ id: string }>;
